80ba9aaa05d7df29345fceb72263a53d96d99ecf
Eunice0520 <euniceyau0520@gmail.com>
2025-04-25 18:17:38 +0800

    Update udid.js

--- api/udid.js
+++ api/udid.js
@@ -1,3 +1,7 @@
 export default function handler(req, res) {
-  res.status(200).json({ msg: "小狐狸Server已啟動！" });
+  if (req.method === "GET") {
+    res.status(200).json({ msg: "小狐狸Server已啟動！" });
+  } else {
+    res.status(405).json({ error: "Method not allowed" });
+  }
 }
